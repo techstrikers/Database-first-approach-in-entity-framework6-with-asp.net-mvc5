869fbd2b80b994defc18fe9aab7ceb74665ac7e3
techstrikers <ajaysaptaputre@techstrikers.com>
2016-01-03 17:01:17 +0530

    CheckIn

---
+++ MVCDatabaseFirst/Controllers/EmployeeController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDatabaseFirst.Models;
+using System.Data.Entity;
+
+namespace MVCDatabaseFirst.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private DBFirstModel db = new DBFirstModel();
+
+        //
+        // GET: /Employee/
+
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+
+        //
+        // GET: /Employee/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        //
+        // GET: /Employee/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Employee/Create
+
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        //
+        // GET: /Employee/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        //
+        // POST: /Employee/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        //
+        // GET: /Employee/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        //
+        // POST: /Employee/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # database-first-approach-in-entity-framework6-with-asp.net-mvc5
+
+In database first approch we creates Entity Data Model, data context and entity classes from an existing database. The beauty of this approch is entity data model can be automatically updated whenever database schema changes. Also, database-first approach supports stored procedure, view, etc.
